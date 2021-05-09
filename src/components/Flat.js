62f9acbacbf013351226813b33053960bcb230d1
Claire Froelich <claire.froelich@gmail.com>
2021-05-09 15:27:35 -0700

    Init commit

---
+++ src/components/Flat.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Flat = (props) => {
+  return (
+    <div className="flat">
+      <div className="flat-picture"></div>
+      <div className="flat-title"></div>
+    </div>
+  );
+};
+
+export default Flat;
